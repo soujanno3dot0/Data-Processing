cb23b41690301bb2fab013ec6828775230ae9da5
soujanno3dot0 <53225472+soujanno3dot0@users.noreply.github.com>
2019-10-19 09:39:27 +0530

    Update importData.py

--- importData.py
+++ importData.py
@@ -1,1 +1,6 @@
 #This page cpature code snippets for importing datasets using python
+Jupyter Notebook
+1. upload the .csv file
+2. once it is uploaded read_csv() in pandas library look into local path so it is not required to give the full path
+3. Code snippet
+    df_dsSurvey = pd.read_csv("Topic_Survey_Assignment.csv")
